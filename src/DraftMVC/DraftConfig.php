50f061faa87a33d2ed0fd9a10151f0dc02649900
Dominic Vonk <dominic@vonk.io>
2020-07-01 21:46:08 +0200

    Added new way of config, to support get env better

--- src/DraftMVC/DraftConfig.php
+++ src/DraftMVC/DraftConfig.php
@@ -1,14 +1,23 @@
 <?php
+
 namespace DraftMVC;
+
 if (!defined('DRAFT_CONFIGS')) {
     define('DRAFT_CONFIGS', __DIR__ . '/config');
 }
-class DraftConfig {
+class DraftConfig
+{
     private static $config;
-    public static function get($path, $default = null) {
+    public static function get($path, $default = null)
+    {
+        if (empty(self::$config)) {
+            foreach (glob(DRAFT_CONFIGS . '/*.php') as $file) {
+                self::$config[pathinfo($file, PATHINFO_FILENAME)] = require($file);
+            }
+        }
         $path = explode('.', $path);
         $config = self::$config;
-        foreach($path as $name) {
+        foreach ($path as $name) {
             if (array_key_exists($name, $config)) {
                 $config = $config[$name];
             } else {
@@ -16,8 +25,5 @@ class DraftConfig {
             }
         }
         return $config;
-    }
-    public static function load($file) {
-        self::$config[pathinfo($file, PATHINFO_FILENAME)] = json_decode(file_get_contents(DRAFT_CONFIGS.'/'.$file), true);
     }
 }
